b9d2d944961df5719bf65eb8f4a9e4ccbb467dcc
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-05-31 17:48:35 +0000

    Added a create blogpost form

--- newsapp/models.py
+++ newsapp/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255, default="newsblog")
+    title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('home')
 
 
