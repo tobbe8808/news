da90d1fc3def10257f8dd18e062149fc2b2b1e53
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-06-02 13:25:15 +0000

    added ckeditor and works on all pages

--- newsapp/models.py
+++ newsapp/models.py
@@ -3,6 +3,7 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -17,7 +18,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='code')
     likes = models.ManyToManyField(User, related_name='blog_posts')
